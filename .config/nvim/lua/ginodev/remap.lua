001a658d2ba896861d368859cc5714a15e00c036
Gino <ginowashere@hotmail.com>
2023-03-25 09:18:50 +1100

    add more mappings

--- .config/nvim/lua/ginodev/remap.lua
+++ .config/nvim/lua/ginodev/remap.lua
@@ -1,7 +1,7 @@
 local opts = { noremap = true, silent = true }
 
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>e", ":NvimTreeFocus<CR>", opts)
+vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
 vim.keymap.set("n", "<leader>p", "\"+p", opts)
 vim.keymap.set("v", "<leader>p", "\"+p", opts)
 vim.keymap.set("n", "<leader>y", "\"+y", opts)
