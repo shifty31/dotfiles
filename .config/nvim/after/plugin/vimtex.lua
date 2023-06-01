030695c9034f784fd16bcb37b8cf378cfc88d460
Gino <ginowashere@hotmail.com>
2023-06-01 18:22:12 +1000

    some remaps

--- .config/nvim/after/plugin/vimtex.lua
+++ .config/nvim/after/plugin/vimtex.lua
@@ -2,3 +2,5 @@ vim.g.vimtex_view_method = 'zathura'
 vim.g.vimtex_view_general_viewer = 'okular'
 vim.g.vimtex_view_general_options = '--unique file:@pdf\\#src:@line@tex'
 vim.g.maplocalleader = ','
+
+vim.g.vimtex_quickfix_open_on_warning = 0
