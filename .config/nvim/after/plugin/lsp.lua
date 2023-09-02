6ff428eda11a2ee9a40010c7d7caaafca8954ab1
Gino <ginowashere@hotmail.com>
2023-09-02 14:25:05 +1000

    swapped pyright for pylsp for breaking nvim

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -13,7 +13,6 @@ lsp.ensure_installed({
 
 lsp.skip_server_setup({ 'help' })
 
--- Fix Undefined global 'vim'
 lsp.configure('lua_ls', {
     settings = {
         Lua = {
@@ -23,6 +22,20 @@ lsp.configure('lua_ls', {
         }
     }
 })
+
+lsp.configure('pylsp', {
+    settings = {
+        pylsp = {
+            plugins = {
+                pycodestyle = {
+                    ignore = { 'W391' },
+                    maxLineLength = 120
+                }
+            }
+        }
+    }
+})
+
 
 lsp.configure('ltex', {
     settings = {
@@ -98,11 +111,9 @@ lsp.on_attach(function(_, bufnr)
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "<leader>do", function() vim.diagnostic.open_float() end, opts)
     vim.keymap.set("n", "<leader>q", function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>vr", function() vim.lsp.buf.references() end, opts)
     vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
     vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 
