1e89ea6ef8cea79929fda8cf09072387378537d9
Gino <ginowashere@hotmail.com>
2024-01-22 15:12:09 +1100

    added keymap for toggling background transparency

--- .config/nvim/lua/plugins/colourscheme.lua
+++ .config/nvim/lua/plugins/colourscheme.lua
@@ -41,6 +41,18 @@ return {
         which_key = true,
       },
     },
+    keys = {
+      {
+        "<leader>bt",
+        function()
+          local cat = require("catppuccin")
+          cat.options.transparent_background = not cat.options.transparent_background
+          cat.compile()
+          vim.cmd.colorscheme(vim.g.colors_name)
+        end,
+        desc = "Toggle background transparency",
+      },
+    },
   },
   {
     "LazyVim/LazyVim",
