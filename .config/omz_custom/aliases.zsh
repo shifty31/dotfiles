13a76b356a29390d55773059a19702e32d5454cc
Gino <ginowashere@hotmail.com>
2024-01-29 12:45:21 +1100

    moved to zsh as default shell and starship
    moved to zsh and base set up with starship as the prompt

---
+++ .config/omz_custom/aliases.zsh
@@ -0,0 +1,9 @@
+
+alias ls='exa --oneline --long --header --icons --all --group-directories-first --no-permissions --no-user --sort=name --modified --time-style=iso'
+alias py='python3'
+alias rmi='rm -rfi'
+alias nv='nvim'
+
+# dotfile management
+alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias lconf='lazygit --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
---
+++ .config/omz_custom/setopt.zsh
@@ -0,0 +1,5 @@
+ZSH_TMUX_AUTOSTART="true"
+
+setopt globdots
+zstyle ':completion:*' special-dirs false
+zstyle ':completion:*' completer _complete _ignored _files
