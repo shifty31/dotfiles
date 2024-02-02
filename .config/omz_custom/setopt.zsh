5e531a0065bddc17969a19823b9c10390b77a2c3
Gino <ginowashere@hotmail.com>
2024-02-02 12:04:37 +1100

    fixedd gitignore not being respected with alt-c

--- .config/omz_custom/setopt.zsh
+++ .config/omz_custom/setopt.zsh
@@ -1,15 +1,17 @@
 ZSH_TMUX_DEFAULT_SESSION_NAME="default"
-# FZF_DEFAULT_COMMAND='fd --hidden --exclude .git --max-depth=1'
+FZF_DEFAULT_COMMAND='fd --hidden --exclude .git'
 FZF_DEFAULT_OPTS='--reverse --border'
+FZF_ALT_C_COMMAND='fd --hidden --exclude .git'
 FZF_CTRL_T_OPTS="
   --preview 'bat -n --color=always {}'
   --bind 'ctrl-/:change-preview-window(down|hidden|)'"
 
-
 # Path adjustments
-#
 path+=(~/.local/bin/)
+
+#zsh opts
 setopt globdots
 
-zstyle ':completion:*' special-dirs false
-zstyle ':completion:*' completer _complete _ignored _files
+#zsh styling
+zstyle ':completion:*' special-dirs false # removes the . and .. from completion
+zstyle ':completion:*' completer _complete _ignored _files # adds file suggests last
