2bf17632429f468d78bb5b8660d3a1f90c9e7e89
Gino <*>
2025-02-09 18:08:02 +1100

    kickstart init

--- .config/nvim/lua/plugins/lualine.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "nvim-lualine/lualine.nvim",
-  opts = function(_, opts)
-    opts.sections.lualine_z = {}
-    return opts
-  end,
-}
--- .config/nvim/lua/plugins/telescope_extensions.lua
+++
@@ -1,9 +0,0 @@
-return {}
--- return {
---
---   "nvim-telescope/telescope-file-browser.nvim",
---   dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
---   keys = { { "<leader>fzz", "Telescope file_browser", desc = "AHHHHHH" } },
--- }
-
--- vim.keymap.set('n', '<leader>fe', require('telescope').extensions.file_browser.file_browser, opts)
